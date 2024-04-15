402910d52149fbe252cdd9661434677385d1f1f6
NikitaDmitruk <neomouz@icloud.com>
2024-04-15 16:09:04 +0300

    Add task with star.

---
+++ src/TaskWithStar/Dog.java
@@ -0,0 +1,15 @@
+package TaskWithStar;
+
+public class Dog {
+
+    private Dog() {
+    }
+
+    public static void eat() {
+        System.out.println("Собака ест мясо!");
+    }
+
+    public static void voice() {
+        System.out.println("Собака лает!");
+    }
+}
---
+++ src/TaskWithStar/Main.java
@@ -0,0 +1,8 @@
+package TaskWithStar;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog.eat();
+        Dog.voice();
+    }
+}
